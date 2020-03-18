be705545c18b8e2479555d7022c2506d5ea6b0a2
shiwx1101 <shiwenxiang999@126.com>
2020-03-18 23:57:54 +0800

    queue

---
+++ src/main/java/org/algorithm/explore/queue/ArrayCircleQueue.java
@@ -0,0 +1,31 @@
+package org.algorithm.explore.queue;
+
+class ArrayQueue{
+    private int capacity;
+    private Object[] items;
+    private int head = 0;
+    private int tail = 0;
+
+    public ArrayQueue(int capacity){
+        this.capacity = capacity;
+        items = new Object[capacity];
+    }
+
+    public boolean enqueue(Object o){
+        if((tail+1)%capacity==head)return false;
+        items[tail] = o;
+        tail = (tail+1)%capacity;
+        return true;
+    }
+
+    public Object dequeue(){
+        if(head==tail)return null;
+        Object o = items[head];
+        head = (head+1)%capacity;
+        return o;
+    }
+
+
+
+
+}
---
+++ src/main/java/org/algorithm/explore/queue/ListQueue.java
@@ -0,0 +1,34 @@
+package org.algorithm.explore.queue;
+
+import org.algorithm.explore.linkedlist.Node;
+class ListQueue<E extends Comparable<E>>{
+    private int capacity;
+    private Node<E> head;
+    private Node<E> tail;
+    private int lenght = 0;
+
+    public ListQueue(int capacity){
+        this.capacity = capacity;
+        this.head = new Node<>();
+        this.tail = head;
+    }
+
+    public boolean enqueue(E e){
+        if(lenght==capacity)return false;
+        Node<E> tmp = new Node<E>(e, null);
+        tail.next = tmp;
+        tail = tmp;
+        ++lenght;
+        return false;
+    }
+
+    public Node<E> dequeue(){
+        if(lenght==0){
+            return null;
+        }
+        Node<E> tmp = head.next;
+        head = tmp;
+        return tmp;
+    }
+
+}
