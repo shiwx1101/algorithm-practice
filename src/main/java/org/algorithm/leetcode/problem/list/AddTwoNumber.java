6ceb28b53e354e1c5c6aae96ad62494a1b492fe7
shiwx1101 <shiwenxiang999@126.com>
2020-07-21 01:53:21 +0800

    update

--- src/main/java/org/algorithm/leetcode/problem/list/AddTwoNumber.java
+++ src/main/java/org/algorithm/leetcode/problem/list/AddTwoNumber.java
@@ -1,27 +1,24 @@
 package org.algorithm.leetcode.problem.list;
 
 /**
- * 
- * 
  * You are given two non-empty linked lists representing two non-negative
  * integers.
- * 
+ * <p>
  * The digits are stored in reverse order and each of their nodes contain a
  * single digit. Add the two numbers and return it as a linked list.
- * 
+ * <p>
  * You may assume the two numbers do not contain any leading zero, except the
  * number 0 itself.
- * 
+ * <p>
  * Example:
- * 
+ * <p>
  * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4) Output: 7 -> 0 -> 8 Explanation: 342 +
  * 465 = 807.
- * 
- * 
- * 
+ * <p>
+ * <p>
+ * <p>
  * public class ListNode { int val; ListNode next; ListNode(int x) { val = x; }
  * }
- * 
  */
 
 public class AddTwoNumber {
@@ -52,12 +49,10 @@ public class AddTwoNumber {
             l1 = l1.next;
             l2 = l2.next;
         }
-
-         //两数相加完成最后还有进位  比如5+5
+        //两数相加完成最后还有进位  比如5+5
         if (remain == 1) {
             result.next = new ListNode(1);
         }
-
         return rNode.next;
     }
 
@@ -75,24 +70,19 @@ public class AddTwoNumber {
 
     }
 
-
-
-    public static void showListNodeVal(ListNode l){
-
-        while(l!=null){
-            System.out.print(l.val+"    ");
-            l=l.next;
+    public static void showListNodeVal(ListNode l) {
+        while (l != null) {
+            System.out.print(l.val + "    ");
+            l = l.next;
         }
-
     }
 
-}
-
-class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode(int x) {
-        val = x;
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+        }
     }
 }
+
