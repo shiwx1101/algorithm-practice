4d3576982b81a03614a97474d9fc90b1f6b681f1
史文祥 <shiwenxiang999@126.com>
2020-08-05 13:51:34 +0800

    fix

--- src/main/java/org/algorithm/leetcode/problem/list/RemoveDuplicatesFromSortedList.java
+++ src/main/java/org/algorithm/leetcode/problem/list/RemoveDuplicatesFromSortedList.java
@@ -20,6 +20,23 @@ package org.algorithm.leetcode.problem.list;
  */
 public class RemoveDuplicatesFromSortedList {
 
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode pre = head;
+        ListNode cur = head.next;
+
+        while (cur != null) {
+            if (pre.val == cur.val) {
+                pre.next = cur.next;
+            } else {
+                pre = pre.next;
+            }
+            cur = cur.next;
+        }
+        return head;
+    }
 
 
     private class ListNode {
---
+++ src/main/java/org/algorithm/leetcode/problem/list/RemoveDuplicatesFromSortedListII.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 2018-2020 XianChengKeJi.Co.Ltd. All Rights Reserved.
+ */
+package org.algorithm.leetcode.problem.list;
+
+/**
+ * Given a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.
+ * <p>
+ * Return the linked list sorted as well.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: 1->2->3->3->4->4->5
+ * Output: 1->2->5
+ * Example 2:
+ * <p>
+ * Input: 1->1->1->2->3
+ * Output: 2->3
+ *
+ * @author shiwenxiang
+ * @version : RemoveDuplicatesFromSortedListII.java, v0.1 2020/8/5 shiwenxiang Exp $$
+ */
+public class RemoveDuplicatesFromSortedListII {
+
+
+}
