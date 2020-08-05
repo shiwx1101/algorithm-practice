7a201405506c754e5f191abd4b1927f27288d977
史文祥 <shiwenxiang999@126.com>
2020-08-05 09:36:40 +0800

    fix

--- src/main/java/org/algorithm/leetcode/problem/list/ReverseNodesInKGroup.java
+++ src/main/java/org/algorithm/leetcode/problem/list/ReverseNodesInKGroup.java
@@ -6,6 +6,23 @@ package org.algorithm.leetcode.problem.list;
 import java.util.List;
 
 /**
+ * Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
+ * <p>
+ * k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes in the end should remain as it is.
+ * <p>
+ * Example:
+ * <p>
+ * Given this linked list: 1->2->3->4->5
+ * <p>
+ * For k = 2, you should return: 2->1->4->3->5
+ * <p>
+ * For k = 3, you should return: 3->2->1->4->5
+ * <p>
+ * Note:
+ * <p>
+ * Only constant extra memory is allowed.
+ * You may not alter the values in the list's nodes, only nodes itself may be changed.
+ *
  * @author shiwenxiang
  * @version 1: ReverseNodesInKGroup.java, v0.1 2020/8/4 shiwenxiang Exp $$
  */
