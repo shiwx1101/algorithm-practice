d63e7c400e8cb307dd58646f87b24b0d39805789
史文祥 <shiwenxiang999@126.com>
2020-08-05 14:30:27 +0800

    删除list重复的元素

--- src/main/java/org/algorithm/leetcode/problem/list/RemoveDuplicatesFromSortedListII.java
+++ src/main/java/org/algorithm/leetcode/problem/list/RemoveDuplicatesFromSortedListII.java
@@ -3,6 +3,8 @@
  */
 package org.algorithm.leetcode.problem.list;
 
+import java.util.List;
+
 /**
  * Given a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.
  * <p>
@@ -21,6 +23,56 @@ package org.algorithm.leetcode.problem.list;
  * @version : RemoveDuplicatesFromSortedListII.java, v0.1 2020/8/5 shiwenxiang Exp $$
  */
 public class RemoveDuplicatesFromSortedListII {
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode prePre = new ListNode(0, head);
+        ListNode p = prePre;
+        ListNode pre = head;
+        ListNode cur = head.next;
+        boolean flag = false;
+        while (cur != null) {
+            if (pre.val == cur.val) {
+                flag = true;
+                pre.next = cur.next;
+                //注意重复的结尾
+                if (cur.next == null) {
+                    prePre.next = null;
+                    break;
+                }
+            } else {
+                if (flag) {
+                    prePre.next = cur;
+                    pre = cur;
+                    flag = false;
+                } else {
+                    prePre = pre;
+                    pre = pre.next;
+                }
+            }
+            cur = cur.next;
+        }
+        return p.next;
+    }
+
+    private class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+
+        public ListNode() {
+        }
+    }
 
 
 }
