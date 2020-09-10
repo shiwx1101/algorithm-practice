2cb38e5a77bb5776570faa91b70c6986f8950b27
史文祥 <shiwenxiang999@126.com>
2020-09-10 21:13:44 +0800

    找出第k大的数据，原地分区

---
+++ src/main/java/org/algorithm/explore/divideandconquer/FindKItem.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 2018-2020 XianChengKeJi.Co.Ltd. All Rights Reserved.
+ */
+package org.algorithm.explore.divideandconquer;
+
+/**
+ * 找出第k大的元素
+ *
+ * @author shiwenxiang
+ * @version : FindKItem.java, v0.1 2020/9/9 shiwenxiang Exp $$
+ */
+public class FindKItem {
+
+    int[] a = {};
+
+
+    public int find(int[] a, int k) {
+        int length = a.length;
+        if (k < 1 || k > length) {
+            throw new IllegalArgumentException();
+        }
+
+
+        return 0;
+    }
+}
--- src/main/java/org/algorithm/explore/recursion/FindKItem.java
+++ src/main/java/org/algorithm/explore/recursion/FindKItem.java
@@ -1,12 +1,11 @@
 package org.algorithm.explore.recursion;
 
 /**
- *
  * 14523
  * 14526
  * 13524
  * 13524
- *
+ * <p>
  * 找出第K大的元素
  */
 public class FindKItem {
@@ -20,22 +19,40 @@ public class FindKItem {
 
         int index = length - 1;
 
-        return findK(a, 0, index, k);
+        return findK(a, 0, index, length - k);
     }
 
     private int findK(int[] a, int from, int to, int k) {
+        int pivot = a[to];
+        int i = from;
+        int j = from;
 
-        int point = to;
-        int f = a[point];
-
-        for (int i = 0; i <= to - from; i++) {
-            if(a[i]>f){
-
+        for (; i < to; i++) {  //原地分区！！
+            if (a[i] < pivot) {
+                swap(a, i, j);
+                j++;
             }
         }
+        swap(a, j, to);
+        if (j == k) {
+            return a[j];
+        } else if (j > k) {
+            return findK(a, from, j - 1, k);
+        } else {
+            return findK(a, j + 1, to, k);
+        }
 
+    }
 
-        return 0;
+    private void swap(int[] a, int i, int j) {
+        int tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {5, 4, 3, 1, 2, 10, 6, 9, 8, 7};
+        System.out.println(new FindKItem().find(a, 1));
     }
 
 }
