c665017b6c3f07fb0b84936f981d19eb1636208e
shiwx1101 <shiwenxiang999@126.com>
2020-09-16 01:47:09 +0800

    fix

--- src/main/java/org/algorithm/explore/tree/TreeMaps.java
+++ src/main/java/org/algorithm/explore/tree/TreeMaps.java
@@ -3,6 +3,9 @@
  */
 package org.algorithm.explore.tree;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * @author shiwenxiang
  * @version : TreeMaps.java, v0.1 2020/9/15 shiwenxiang Exp $$
@@ -13,46 +16,71 @@ public class TreeMaps<K extends Comparable, V> {
 
     private static final boolean BLACK = false;
 
-    private TreeNode h;
+    private TreeNode root;
 
-    public TreeNode put(K k, V v) {
+    public void put(K k, V v) {
+        root = put(root, k, v);
+        root.color = BLACK;
+    }
+
+    public TreeNode put(TreeNode h, K k, V v) {
         if (h == null) {
             return new TreeNode(k, v, RED, 1);
         }
         int cmp = k.compareTo(h.key);
         if (cmp > 0) {
-            h.right = put(k, v);
+            h.right = put(h.right, k, v);
         } else if (cmp < 0) {
-            h.left = put(k, v);
+            h.left = put(h.left, k, v);
         } else {
             h.value = v;
         }
         if (isRed(h.right) && !isRed(h.left)) {
-            rotateLeft(h);
+            h = rotateLeft(h);
         }
         if (isRed(h.left) && isRed(h.left.left)) {
-            rotateRight(h);
+            h = rotateRight(h);
         }
         if (isRed(h.left) && isRed(h.right)) {
             flipColors(h);
         }
         h.N = size(h.left) + size(h.right) + 1;
-        return null;
+        return h;
     }
 
     private int size(TreeNode left) {
-        return 0;
+        if (left == null) return 0;
+        return size(left.right) + size(left.left) + 1;
     }
 
     private void flipColors(TreeNode h) {
+        h.color = RED;
+        h.left.color = BLACK;
+        h.right.color = BLACK;
+    }
+
+    private TreeNode rotateRight(TreeNode h) {
+        TreeNode x = h.left;
+        h.left = x.right;
+        x.right = h;
+        x.color = h.color;
+        h.color = RED;
+        x.N = h.N;
+        h.N = 1 + size(h.left) + size(h.right);
+        return x;
 
     }
 
-    private void rotateRight(TreeNode h) {
+    private TreeNode rotateLeft(TreeNode h) {
 
-    }
-
-    private void rotateLeft(TreeNode h) {
+        TreeNode x = h.right;
+        h.right = x.left;
+        x.left = h;
+        x.color = h.color;
+        h.color = RED;
+        x.N = h.N;
+        h.N = 1 + size(h.left) + size(h.right);
+        return x;
     }
 
 
@@ -77,5 +105,13 @@ public class TreeMaps<K extends Comparable, V> {
         return x.color;
     }
 
+    public static void main(String[] args) {
+        TreeMaps<Integer, Integer> maps = new TreeMaps<>();
+        Stream.of(1,2,3,4,5).collect(Collectors.toList()).forEach(it->{
+            maps.put(it,it);
+        });
+        System.out.println("------------");
+        System.out.println(maps.toString());
+    }
 
 }
