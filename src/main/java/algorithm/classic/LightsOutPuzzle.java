ee429492c0cb258bbbf2546e33c4a354cec74f4c
shiwx1101 <shiwenxiang999@126.com>
2020-11-10 08:39:19 +0800

    熄灯问题

---
+++ src/main/java/algorithm/classic/LightsOutPuzzle.java
@@ -0,0 +1,36 @@
+package algorithm.classic;
+
+/**
+ * 熄灯问题
+ */
+public class LightsOutPuzzle {
+
+    int[][] lights;
+
+    int[][] action;
+
+    int[][] result;
+
+    public LightsOutPuzzle(int[][] lights) {
+        this.lights = lights;
+    }
+
+    void action(){
+//        int length = lights.length;
+        for (int i = 0; i < action[0].length; i++) {
+            int[] solution = new int[action[0].length];
+            int temp = i;
+            for (int j = solution.length-1; j >=0 ; j--) {
+
+            }
+
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
